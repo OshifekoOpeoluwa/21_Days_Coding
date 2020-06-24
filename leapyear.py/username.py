85c2fc66858c895e0c11b3fd561dd6380edc27cb
OshifekoOpeoluwa <oshifekoopeoluwa@gmail.com>
2020-06-24 23:36:35 +0100

    hello world

---
+++ leapyear.py/username.py
@@ -0,0 +1,13 @@
+#function to create a user email
+
+username = input ("accept user email:")
+print("oshifekoopeoluwa@gmail.com")
+
+username = input ("accept user first name:")
+print("Opeoluwa")
+
+username = input ("accept user last name :")
+print("Oshifeko")
+
+username = input ("accept user phone number:")
+print("08033779935")
